4232c403cd0907481018b8660ef5221109eaf2d6
Sang Saephan <sangsaephan510@yahoo.com>
2017-12-25 10:25:31 -0800

    Set up TableView delegate and datasource methods

---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Sang Saephan on 12/19/17.
+//  Copyright © 2017 Sang Saephan. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if cell?.accessoryType == UITableViewCellAccessoryType.none {
+            cell?.accessoryType = .checkmark
+        } else {
+            cell?.accessoryType = .none
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+
+}
+
